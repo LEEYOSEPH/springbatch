0a2452ef30bdc301681f90a07e09ca8343637b5d
LEEYOSEPH <laksmi0628@gmail.com>
2023-02-13 14:59:01 +0900

    Initial commit

---
+++ src/main/java/com/example/springbach/SpringbachApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbach/SpringbachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
